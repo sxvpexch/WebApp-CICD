22b6f98817f5ea2ac9164c7583aa86f2f0a4ea3f
Khushi Sharma <ks483020@gmail.com>
2024-02-17 11:07:27 +0530

    networking files to repo

--- terraform_base/networking/networking.tf
+++ terraform_base/networking/networking.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "main-vpc" {
     Name = "student.7-vpc"
   }
 }
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.main-vpc.id
+  cidr_block = "10.7.0.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+     Name = "student.7-subnet-1"
+  }
+}
