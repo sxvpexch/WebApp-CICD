6b4427f2a891f8b6a434b624e9f51af200cc26ad
Khushi Sharma <ks483020@gmail.com>
2024-02-17 11:47:45 +0530

    adding aws network files

--- terraform_base/networking/networking.tf
+++ terraform_base/networking/networking.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "main-vpc" {
     Name = "student.7-vpc"
   }
 }
+
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.main-vpc.id
   cidr_block = "10.7.0.0/24"
@@ -12,4 +13,29 @@ resource "aws_subnet" "subnet-1" {
   tags = {
      Name = "student.7-subnet-1"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+ vpc_id = aws_vpc.main-vpc.id
+ tags = {
+   Name = "student.3-igw"
+ }
+}
+
+resource "aws_route_table" "my-route-table" {
+ vpc_id = aws_vpc.main-vpc.id
+ 
+ route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+ }
+ 
+ tags = {
+      Name = "student.7-route-table"
+ }
+ }
+
+resource "aws_route_table_association" "my-route-association" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.my-route-table.id
 }
--- terraform_base/networking/output.tf
+++ terraform_base/networking/output.tf
@@ -5,3 +5,15 @@ output "my_vpc" {
 output "my_subnet" {
  value = aws_subnet.subnet-1.id
 }
+
+output "my_gateway" {
+ value = aws_internet_gateway.igw.id
+}
+
+output "my_route_table" {
+ value = aws_route_table.my-route-table.id
+}
+
+output "my_route_table_association" {
+ value = aws_route_table_association.my-route-association.id
+}
