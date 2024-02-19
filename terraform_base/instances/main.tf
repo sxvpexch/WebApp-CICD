b0d0c475c2855d33cbd2c6f83f835a2e49baf8c0
Khushi Sharma <ks483020@gmail.com>
2024-02-19 12:46:39 +0530

    working vm

--- terraform_base/instances/main.tf
+++ terraform_base/instances/main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "my_vm" {
       ami = "ami-0c7217cdde317cfec"
       subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
       key_name = data.terraform_remote_state.network_details.outputs.my_key
+      vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
       instance_type = "t2.micro"
       tags = {
  	     Name = "student.7-vm1"
