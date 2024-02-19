01fdcf11331b90d6ea5e41c3b253e36f58db00b4
Khushi Sharma <ks483020@gmail.com>
2024-02-19 10:22:42 +0530

    end of lab, launching vms

--- terraform_base/instances/main.tf
+++ terraform_base/instances/main.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "network_details" {
   region = "us-east-1"
  }
 }
+
+resource "aws_instance" "my_vm" {
+      ami = "ami-0c7217cdde317cfec"
+      subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+      instance_type = "t2.micro"
+      tags = {
+ 	     Name = "student.7-vm1"
+      }
+}
