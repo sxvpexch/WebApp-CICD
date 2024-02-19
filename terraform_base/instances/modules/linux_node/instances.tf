17d32253b30ee27a36744255122f84c841516719
Khushi Sharma <ks483020@gmail.com>
2024-02-19 16:02:15 +0530

    adding schn

---
+++ terraform_base/instances/modules/linux_node/instances.tf
@@ -0,0 +1,20 @@
+data "terraform_remote_state" "network_details" {
+ backend = "s3"
+ config = {
+  bucket = "student.7-khushi-bucket"
+  key = "student.7-network-state"
+  region = "us-east-1"
+ }
+}
+
+resource "aws_instance" "my_vm" {
+      ami = "ami-0c7217cdde317cfec"
+      subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+      key_name = data.terraform_remote_state.network_details.outputs.my_key
+      vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+      instance_type = "t2.micro"
+      tags = {
+             Name = "student.7-vm1"
+      }
+}
+
